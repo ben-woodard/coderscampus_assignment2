b3b096d5108d914e45d6c992e8929b1e82d245ba
Ben Woodard <benjaminwoodard93@gmail.com>
2023-11-17 09:04:20 -0500

    refactored

--- src/com/coderscampus/assignment2/GuessingGame.java
+++ src/com/coderscampus/assignment2/GuessingGame.java
@@ -9,34 +9,34 @@ public class GuessingGame {
 		Random randomGenerator = new Random();
 		int gameNumber = randomGenerator.nextInt(1, 101);
 
+		Scanner gameScanner = new Scanner(System.in);
+
 		int userGuess = 0;
 		int guessCount = 0;
 		while (guessCount < 5) {
 			System.out.println("Pick a number between 1 and 100");
-			Scanner gameScanner = new Scanner(System.in);
 			userGuess = Integer.parseInt(gameScanner.nextLine());
 
-			if (guessCount < 5) {
-				if (userGuess < 1 || userGuess > 100) {
-					System.out.println("Your guess is not between 1 and 100, please try again");
-					continue;
-				}
-				if (userGuess == gameNumber) {
-					System.out.println("You win!");
-				} else if (userGuess < gameNumber) {
-					System.out.println("Please pick a higher number");
-
-				} else if (userGuess > gameNumber) {
-					System.out.println("Please pick a lower number");
-				}
-				guessCount++;
+			if (userGuess < 1 || userGuess > 100) {
+				System.out.println("Your guess is not between 1 and 100, please try again");
+				continue;
 			}
-
-			if (userGuess != gameNumber) {
-				System.out.println("You lose!");
-				System.out.println("The number was: " + gameNumber);
+			if (userGuess == gameNumber) {
+				System.out.println("You win!");
+				break;
+			} else if (guessCount == 4) {
+				break;
+			} else if (userGuess < gameNumber) {
+				System.out.println("Please pick a higher number");
+			} else if (userGuess > gameNumber) {
+				System.out.println("Please pick a lower number");
 			}
-			gameScanner.close();
+			guessCount++;
 		}
+		if (userGuess != gameNumber) {
+			System.out.println("You lose!");
+			System.out.println("The number was: " + gameNumber);
+		}
+		gameScanner.close();
 	}
 }
